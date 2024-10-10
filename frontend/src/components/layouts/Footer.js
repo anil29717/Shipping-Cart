ef3fd1037649734b337a62aa1690b77a7f33ad8a
anil <anil29717@gmail.com>
2024-10-10 13:05:37 +0530

    shipping-cart

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+export  default function Footer (){
+    return (
+        <footer className="py-1">
+            <p className="footer-section">
+                Sipping Cart - 2024, All Rights Reserved | Made by Anil Kumar
+            </p>
+        </footer>
+    )
+}
